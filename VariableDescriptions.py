629661b27ee14be7cda4b357127120164e9b9ab8
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2024-09-14 20:57:42 -0400

    Create VariableDescriptions.py
    Lists all of the variables in Granot and Sari.

---
+++ VariableDescriptions.py
@@ -0,0 +1,57 @@
+nu_sa
+nu_m
+nu_c
+
+
+k = 0, 2
+# radial dependence of the ambient medium
+# density is proportional to r^(-k)
+# k = 0, constant density, k = 2, 1/r^2
+
+b = 
+beta_1 =
+beta_2 = 
+
+
+p = 2.5
+# represents the spectral index of the electron distribution
+# number of electrons with energy E is proportional to E^(-p)
+# range between 2 - 4, 2.5 preferred value.
+
+z = 1 
+# cosmological red shift
+
+
+epsilon_e = 
+# fraction of the total energy density in electrons
+# between 10^-6 and 0.4
+
+epsilon_e_bar = (epsilon_e*(p-2)) / (p-1)
+
+
+epsilon_B = 
+# fraction total energy density in the magnetic field 
+# between 10^-9 and 0.3
+
+epsilon_p = 1 - (epsilon_e + epsilon_B)
+
+
+n_0 = 
+# ambient density in units of particles/cm^3
+# height of the density function for k = 0
+# between 10^-4 to 10^3
+
+E_52 = 
+# explosion energy of the GRB in units of 10^52 ergs
+# between 10^-4 and 10^2
+
+A_star = 
+# height of the density function for k = 2
+
+t_day = 
+# time since explosion in units of days in the observer frame(?)
+# 0.01 and 100
+
+d_L28 = 
+# luminosity distance in units of 10^28 cm
+# ned wright cosmology calculator to convert z to d_L28
