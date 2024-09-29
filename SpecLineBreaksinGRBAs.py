21a47ed33caaf2c243a261411b478b96b83bc94a
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2024-09-28 23:48:24 -0400

    Update SpecLineBreaksinGRBAs.py
    Improved code readability
    
    removed _b_ dummy variable notation in each case for BreakCase()

--- SpecLineBreaksinGRBAs.py
+++ SpecLineBreaksinGRBAs.py
@@ -64,79 +64,74 @@ def BreakCase(b,k):
 			beta_2 = (1-p)/2
 			nu_b = nu_m
 
-			var_2_1 = (p - 0.67) * (10**15)
-			var_2_2 = pow((1 + z), 1/2)
-			var_2_3 = pow(E_52, 1/2)
-			var_2_4 = pow(epsilon_e_bar, 2)
-			var_2_5 = pow(epsilon_B, 1/2)
-			var_2_6 = pow(t_days, -3/2)
+			var1 = (p - 0.67) * (10**15)
+			var2 = pow((1 + z), 1/2)
+			var3 = pow(E_52, 1/2)
+			var4 = pow(epsilon_e_bar, 2)
+			var5 = pow(epsilon_B, 1/2)
+			var6 = pow(t_days, -3/2)
 
-			nu_2 = 3.73 * var_2_1 * var_2_2 * var_2_3 * var_2_4 * var_2_5 * var_2_6
+			nu_2 = 3.73 * var1 * var2 * var3 * var4 * var5 * var6
 
 		case(b = 3, k = 0):
 			beta_1 = (1 - p)/2
 			beta_2 = -p/2
 			nu_b = nu_c
 
-			var_3_1 = (p - 0.46) * (10**13)
-			var_3_2 = pow(e, -1.16*p) # is the e here the function e or something else
+			var1 = (p - 0.46) * (10**13)
+			var2 = pow(e, -1.16*p) # is the e here the function e or something else
 			# if it is exponential function: math.exp(-1.16*p)
-			var_3_3 = pow((1 + z), -1/2)
-			var_3_4 = pow(epsilon_B, -3/2)
-			var_3_5 = pow(n_0, -1)
-			var_3_6 = pow(E_52, -1/2)
-			var_3_7 = pow(t_days, -1/2)
+			var3 = pow((1 + z), -1/2)
+			var4 = pow(epsilon_B, -3/2)
+			var5 = pow(n_0, -1)
+			var6 = pow(E_52, -1/2)
+			var7 = pow(t_days, -1/2)
 		              
-			nu_3 = 6.37 * var_3_1 * var_3_2 * var_3_3 * var_3_4 * var_3_5 * var_3_6 * var_3_7
+			nu_3 = 6.37 * var1 * var2 * var3 * var4 * var5 * var6 * var7
 
 		case(b = 7, k = 0):
-			#dummy variables for writing nu_7
-			var_7_1 = ( ((3p-1)**(8/5)) / ((3p+2)**(8/5)) )
-			var_7_2 = (1+z)**(-13/10)
-			var_7_3 = (epsilon_e_bar**(-8/5))
-			var_7_4 = (epsilon_B**(-2/5))
-			var_7_5 = (n_0**(3/10))
-			var_7_6 = (E_52**(-1/10))
-			var_7_7 = (t_day**(3/10))
+			#dummy variables for writing nu_7, E.G. var_1 = var_7_1
+			var1 = ( ((3p-1)**(8/5)) / ((3p+2)**(8/5)) )
+			var2 = (1+z)**(-13/10)
+			var3 = (epsilon_e_bar**(-8/5))
+			var4 = (epsilon_B**(-2/5))
+			var5 = (n_0**(3/10))
+			var6 = (E_52**(-1/10))
+			var7 = (t_day**(3/10))
 
 			#Given by table 2 for b = 7
 			beta_1 = 2
 			beta_2 = 11/8
 
-			#I think there is an error here maybe. nu_b = nu_ac = nu_7
-			nu_b = nu_ac
-
 			#function given by nu_b of granot and sari
-			nu_7 = 1.12 * var_7_1 * (10**8) * var_7_2 * var_7_3 * var_7_4 * var_7_5 * var_7_6 * var_7_7
+			nu_7 = 1.12 * var1 * (10**8) * var2 * var3 * var4 * var5 * var6 * var7
 
-			#nu_b for the given case of b = 7
-			nu_b = nu_7
-
-			return nu_b
+			#nu_b for the given case of b = 7, represents nu_ac
+			return nu_7, beta_1, beta_2, s
 
 		case(b = 9, k = 0):
-			#dummy variables to shorten nu_10
-			var_9_1 = (p - 0.74)
-			var_9_2 = (1 + z)**(1/2)
-			var_9_3 = epsilon_e_bar**2
-			var_9_4 = epsilon_B**(1/2)
-			var_9_5 = E_52**(1/2)
-			var_9_6 = t_days**(-3/2)
+			#dummy variables to shorten nu_9
+			var1 = (p - 0.74)
+			var2 = (1 + z)**(1/2)
+			var3 = epsilon_e_bar**2
+			var4 = epsilon_B**(1/2)
+			var5 = E_52**(1/2)
+			var6 = t_days**(-3/2)
 
-			nu_9 = 3.94 * var_9_1 * 10**15 * var_9_2 * var_9_3 * var_9_4 * var_9_5 * var_9_6
+			nu_9 = 3.94 * var1 * 10**15 * var2 * var3 * var4 * var5 * var6
 			beta_1 = -1/2
 			beta_2 = -p/2
 			nu_b = nu_m
 
 		case(b = 10, k = 0):
 			#dummy variables to shorten nu_10
-			var_10_1 = (1+z)**(-1/2)
-			var_10_2 = epsilon_B**(6/5)
-			var_10_3 = n_0**(-1)
-			var_10_4 = E_52**(7/10)
-			var_10_5 = t_days**(-1/2)
+			var1 = (1+z)**(-1/2)
+			var2 = epsilon_B**(6/5)
+			var3 = n_0**(-1)
+			var4 = E_52**(7/10)
+			var5 = t_days**(-1/2)
 
-			nu_10 = 1.32 * 10**10 * var_10_1 * var_10_2 * var_10_3 * var_10_4 * var_10_5 * 
+			nu_10 = 1.32 * 10**10 * var1 * var2 * var3 * var4 * var5 
 
 			beta_1 = 11/8
 			beta_2 = 1/3
@@ -147,19 +142,18 @@ def BreakCase(b,k):
 			beta_2 = -1/2
 
 			#dummy variables to shorten nu_11
-			var_11_1 = pow((1+z), -1/2)
-			var_11_2 = pow(epsilon_B, -3/2)
-			var_11_3 = pow(n_0, -1)
-			var_11_4 = pow(E_52, -1/2)
-			var_11_5 = pow(t_days, -1/2)
+			var1 = pow((1+z), -1/2)
+			var2 = pow(epsilon_B, -3/2)
+			var3 = pow(n_0, -1)
+			var4 = pow(E_52, -1/2)
+			var5 = pow(t_days, -1/2)
 
-			nu_11 = 5.86 * 10**12 * var_11_1 * var_11_2 * var_11_3 * var_11_4 * var_11_5
+			nu_11 = 5.86 * 10**12 * var1 * var2 * var3 * var4 * var5
 
 		case _:
 			print("Error: Invalid option for b, k")
 			break
 			#invalid option for b, k
-
 
 
 # External flux density
@@ -197,13 +191,13 @@ def BreakCase(b,k):
 
 # Corresponding flux densities
 
-def F(nu_b_ext,nu,nu_b,s,beta_1,beta_2):
+def F(nu_b_ext, nu, nu_b, s, beta_1, beta_2):
     return nu_b_ext*(((nu/nu_b)**(-s*beta_1))+((nu/nu_b)**(-s*beta_2))**(-1/s))  
     #KP - Lily I think you are missing a parenthasis
 	# implementation of eqn 1
 	# function = flux at desired frequency, with passed argument of nu_b_ext, nu_b, s, beta_1, beta_2
 
-def F_tilde(nu,nu_b,s,beta_1,beta_2):
+def F_tilde(nu, nu_b, s, beta_1, beta_2):
     return (1+(nu/nu_b)**(s*(beta_1-beta_2)))**(-1/s)
 	# implementation of eqn 4, with passed argument of nu_b, s, beta_1, beta_2
 
