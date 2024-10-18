a7285ec13403d8a0a9cced838f7dd19489954d2f
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2024-10-18 13:10:12 -0400

    Update SpecLineBreaksinGRBAs.py
    Implemented flux density into BreakCase

--- SpecLineBreaksinGRBAs.py
+++ SpecLineBreaksinGRBAs.py
@@ -1,3 +1,15 @@
+###################################################
+#
+#
+#
+#
+#
+#
+###################################################
+
+
+
+
 import math
 
 k = 0,2
@@ -6,9 +18,6 @@ k = 0,2
 b = 1,2,3,7,9,10,11
 # possible values of b. Method of selection is not considered, could be input() if desired
 
-nu = 
-#frequency 
-
 p = 2.5
 # represents the spectral index of the electron distribution
 # number of electrons with energy E is proportional to E^(-p)
@@ -16,6 +25,10 @@ p = 2.5
 
 z = 1 
 # cosmological red shift
+
+nu =
+# range between 10 MHz and 1 TeV
+# convert energies into frequencies
 
 epsilon_e = range(10**(-6), 0.4)
 # fraction of the total energy density in electrons
@@ -44,8 +57,65 @@ d_L28 =
 # luminosity distance in units of 10^28 cm
 # ned wright cosmology calculator to convert z to d_L28
 
+# Corresponding flux densities
+
+def F(nu_b_ext, nu, nu_b, s, beta_1, beta_2):
+    return nu_b_ext*(((nu/nu_b)**(-s*beta_1))+((nu/nu_b)**(-s*beta_2))**(-1/s))  
+    #KP - Lily I think you are missing a parenthasis
+	# implementation of eqn 1
+	# function = flux at desired frequency, with passed argument of nu_b_ext, nu_b, s, beta_1, beta_2
+
+def F_tilde(nu, nu_b, s, beta_1, beta_2):
+    return (1+(nu/nu_b)**(s*(beta_1-beta_2)))**(-1/s)
+	# implementation of eqn 4, with passed argument of nu_b, s, beta_1, beta_2
+
+def F5(nu,s,beta_1,beta_2):
+    return F(nu_1_ext,nu,nu_1,s,beta_1,beta_2)*F_tilde(nu,nu_2,s,beta_1,beta_2)*F_tilde(nu,nu_3,s,beta_1,beta_2)
+	# implementation of eqn 5, with passed argument of nu, s, beta_1, beta_2
+
+def F9(nu,s,beta_1,beta_2):
+    return F(nu_7_ext,nu,nu_7,s,beta_1,beta_2)*F_tilde(nu,nu_10,s,beta_1,beta_2)*F_tilde(nu,nu_11,s,beta_1,beta_2)*F_tilde(nu,nu_9,s,beta_1,beta_2)
+	# implementation of eqn 9, with passed argument of nu, s, beta_1, beta_2
+
+
 # Function for defining the break frequencies
-def BreakCase(b,k):
+
+#KP- I tried to add all the functions needed in one case break. I don't know if it's right or not. I am going to add it below as a comment:
+def BreakCase(beta_1, beta_2, s, nu_b, nu, b, k):
+	match(beta_1, beta_2, s, nu_b, nu, b, k):
+		case(beta_1 = 2, beta_2 = 1/3, s = 1.64 nu_b = nu_sa, b = 1, k = 0):
+
+			#this case gives out the nu_b, F_tilde, F_ext, and F_nu for break 1 of k = 0
+
+			var1 = (pow((p - 1), 3/5)) / (pow((3*p + 2), 3/5))
+			var2 = pow((1 + z), -1)
+			var3 = pow(epsilon_e_bar, -1)
+			var4 = pow(epsilon_B, 1/5)
+			var5 = pow(n_0, 3/5)
+			var6 = pow(E_52, 1/5)
+
+			nu_sa = 1.24 * var1 * (10**9) * var2 * var3 * var4 * var5 * var6
+			F_tilde = (1 + pow((nu/nu_sa), s*(beta_1 - beta_2)))**(-1/s)
+			
+			#KP - I tried and included the ext flux density equation in this break case
+			var_1_ext_1 = (pow((p - 1), 6/5)) / ((3*p - 1)*(pow((3*p + 2), 1/5)))
+			var_1_ext_2 = pow((1 + z), 1/2)
+			var_1_ext_3 = pow(epsilon_e_bar, -1)
+			var_1_ext_4 = pow(epsilon_B, 2/5)
+			var_1_ext_5 = pow(n_0, 7/10)
+			var_1_ext_6 = pow(E_52, 9/10)
+			var_1_ext_7 = pow(t_days, 1/2)
+			var_1_ext_8 = pow(d_L28, -2)
+
+			nu_1_ext = 0.647 * var_1_ext_1 * var_1_ext_2 * var_1_ext_3 * var_1_ext_4 * var_1_ext_5 * var_1_ext_6 * var_1_ext_7 * var_1_ext_8
+
+			F_nu = nu_1_ext * (pow((nu/nu_b), -s*beta_1) + pow((nu/nu_b), -s*beta_2))**(-1/s)
+			return F_tilde, F_nu
+
+
+
+'''
+def BreakCase(b,k): 
 	match(b,k):
 		case(b = 1, k = 0):
 			beta_1 = 2
@@ -151,12 +221,14 @@ def BreakCase(b,k):
 			var4 = pow(E_52, -1/2)
 			var5 = pow(t_days, -1/2)
 
-			nu_11 = 5.86 * 10**12 * var1 * var2 * var3 * var4 * var5
+			nu_11 = 5.86 * 10**12 * var1 * var2 * var3 * var4 * var_11_5
 
 		case _:
 			print("Error: Invalid option for b, k")
 			break
 			#invalid option for b, k
+'''
+
 
 
 # External flux density
@@ -191,55 +263,5 @@ def BreakCase(b,k):
 
 #nu_7_ext = 5.27 * pow(10, -3) * var_7_ext_1 * var_7_ext_2 * var_7_ext_3 * var_7_ext_4 * var_7_ext_5 * var_7_ext_6 * var_7_ext_7 * var_7_ext_8
 
-#KP- I tried to add all the functions needed in one case break. I don't know if it's right or not. I am going to add it below as a comment:
-'''
-def BreakCase(beta_1, beta_2, s, nu_b, b, k):
-	match(beta_1, beta_2, s, nu_b, b, k):
-		case(beta_1 = 2, beta_2 = 1/3, s = 1.64 nu_b = nu_sa, b = 1, k = 0):
 
-			#this case gives out the nu_b, F_tilde, F_ext, and F_nu for break 1 of k = 0
 
-			var1 = (pow((p - 1), 3/5)) / (pow((3*p + 2), 3/5))
-			var2 = pow((1 + z), -1)
-			var3 = pow(epsilon_e_bar, -1)
-			var4 = pow(epsilon_B, 1/5)
-			var5 = pow(n_0, 3/5)
-			var6 = pow(E_52, 1/5)
-
-			nu_sa = 1.24 * var1 * (10**9) * var2 * var3 * var4 * var5 * var6
-			F_tilde = (1 + pow((nu/nu_sa), s*(beta_1 - beta_2)))**(-1/s)
-			
-			#KP - I tried and included the ext flux density equation in this break case
-			var_1_ext_1 = (pow((p - 1), 6/5)) / ((3*p - 1)*(pow((3*p + 2), 1/5)))
-			var_1_ext_2 = pow((1 + z), 1/2)
-			var_1_ext_3 = pow(epsilon_e_bar, -1)
-			var_1_ext_4 = pow(epsilon_B, 2/5)
-			var_1_ext_5 = pow(n_0, 7/10)
-			var_1_ext_6 = pow(E_52, 9/10)
-			var_1_ext_7 = pow(t_days, 1/2)
-			var_1_ext_8 = pow(d_L28, -2)
-
-			nu_1_ext = 0.647 * var_1_ext_1 * var_1_ext_2 * var_1_ext_3 * var_1_ext_4 * var_1_ext_5 * var_1_ext_6 * var_1_ext_7 * var_1_ext_8
-
-			F_nu = nu_1_ext * (pow((nu/nu_b), -s*beta_1) + pow((nu/nu_b), -s*beta_2))**(-1/s)
-'''
-
-# Corresponding flux densities
-
-def F(nu_b_ext, nu, nu_b, s, beta_1, beta_2):
-    return nu_b_ext*(((nu/nu_b)**(-s*beta_1))+((nu/nu_b)**(-s*beta_2))**(-1/s))  
-    #KP - Lily I think you are missing a parenthasis
-	# implementation of eqn 1
-	# function = flux at desired frequency, with passed argument of nu_b_ext, nu_b, s, beta_1, beta_2
-
-def F_tilde(nu, nu_b, s, beta_1, beta_2):
-    return (1+(nu/nu_b)**(s*(beta_1-beta_2)))**(-1/s)
-	# implementation of eqn 4, with passed argument of nu_b, s, beta_1, beta_2
-
-def F5(nu,s,beta_1,beta_2):
-    return F(nu_1_ext,nu,nu_1,s,beta_1,beta_2)*F_tilde(nu,nu_2,s,beta_1,beta_2)*F_tilde(nu,nu_3,s,beta_1,beta_2)
-	# implementation of eqn 5, with passed argument of nu, s, beta_1, beta_2
-
-def F9(nu,s,beta_1,beta_2):
-    return F(nu_7_ext,nu,nu_7,s,beta_1,beta_2)*F_tilde(nu,nu_10,s,beta_1,beta_2)*F_tilde(nu,nu_11,s,beta_1,beta_2)*F_tilde(nu,nu_9,s,beta_1,beta_2)
-	# implementation of eqn 9, with passed argument of nu, s, beta_1, beta_2
