ea4857c53d93d0511d23ca4035c82fbf51b24d96
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2024-10-25 10:39:21 -0400

    Update SpecLineBreaksinGRBAs.py
    Finished the rest of the cases for BreakCase(): and removed the old BreakCase(): from the comments.
    
    Missing some values from Granot and Sari and I haven't added the Flux Densities yet
    
    Made a lot more comments,
    Fixed some spacing issues that would make Python cry,
    Made it a little more readable.

--- SpecLineBreaksinGRBAs.py
+++ SpecLineBreaksinGRBAs.py
@@ -61,23 +61,23 @@ d_L28 =
 ##############################################################
 '''
 def F(nu_b_ext, nu, nu_b, s, beta_1, beta_2):
-    return nu_b_ext*(((nu/nu_b)**(-s*beta_1))+((nu/nu_b)**(-s*beta_2))**(-1/s)) 
+	return nu_b_ext*(((nu/nu_b)**(-s*beta_1))+((nu/nu_b)**(-s*beta_2))**(-1/s)) 
 	# implementation of eqn 1
 	# function = flux at desired frequency, with passed argument of nu_b_ext, nu_b, s, beta_1, beta_2
 
 def F_tilde(nu, nu_b, s, beta_1, beta_2):
-    return (1+(nu/nu_b)**(s*(beta_1-beta_2)))**(-1/s)
+	return (1+(nu/nu_b)**(s*(beta_1-beta_2)))**(-1/s)
 	# implementation of eqn 4, with passed argument of nu_b, s, beta_1, beta_2
 '''
 
 
 # Needs to be rewritten to align the new BreakCase():
 def F5(nu,s,beta_1,beta_2):
-    return F(nu_1_ext,nu,nu_1,s,beta_1,beta_2)*F_tilde(nu,nu_2,s,beta_1,beta_2)*F_tilde(nu,nu_3,s,beta_1,beta_2)
+	return F(nu_1_ext,nu,nu_1,s,beta_1,beta_2)*F_tilde(nu,nu_2,s,beta_1,beta_2)*F_tilde(nu,nu_3,s,beta_1,beta_2)
 	# implementation of eqn 5, with passed argument of nu, s, beta_1, beta_2
 
 def F9(nu,s,beta_1,beta_2):
-    return F(nu_7_ext,nu,nu_7,s,beta_1,beta_2)*F_tilde(nu,nu_10,s,beta_1,beta_2)*F_tilde(nu,nu_11,s,beta_1,beta_2)*F_tilde(nu,nu_9,s,beta_1,beta_2)
+	return F(nu_7_ext,nu,nu_7,s,beta_1,beta_2)*F_tilde(nu,nu_10,s,beta_1,beta_2)*F_tilde(nu,nu_11,s,beta_1,beta_2)*F_tilde(nu,nu_9,s,beta_1,beta_2)
 	# implementation of eqn 9, with passed argument of nu, s, beta_1, beta_2
 
 ##############################################################
@@ -92,10 +92,14 @@ def F9(nu,s,beta_1,beta_2):
 #KP- I tried to add all the functions needed in one case break. I don't know if it's right or not. I am going to add it below as a comment:
 def BreakCase(b, k, beta_1, beta_2, s, nu_b, nu):
 	match(b, k, beta_1, beta_2, s, nu_b, nu):
+
+
+		# Break 1 for k = 0
 		case(b = 1, k = 0, beta_1 = 2, beta_2 = 1/3, s = 1.64 nu_b = nu_sa):
 
-		    #This is for break = 1 of k = 0
-
+			# Dummy variables for calculating nu_b | nu_b = nu_
+			# Dummy variation notation will follow as such for all break cases:
+			# var_i = var_x_i for b = x and i being the iteration, x is understood by the case of BreakCase():
 			var1 = (pow((p - 1), 3/5)) / (pow((3*p + 2), 3/5))
 			var2 = pow((1 + z), -1)
 			var3 = pow(epsilon_e_bar, -1)
@@ -106,7 +110,7 @@ def BreakCase(b, k, beta_1, beta_2, s, nu_b, nu):
 			nu_sa = 1.24 * var1 * (10**9) * var2 * var3 * var4 * var5 * var6
 			
 
-			#The chunk below solves for F_nu equation 1
+			# The chunk below solves for F_nu equation 1
 			var_1_ext_1 = (pow((p - 1), 6/5)) / ((3*p - 1)*(pow((3*p + 2), 1/5)))
 			var_1_ext_2 = pow((1 + z), 1/2)
 			var_1_ext_3 = pow(epsilon_e_bar, -1)
@@ -122,11 +126,14 @@ def BreakCase(b, k, beta_1, beta_2, s, nu_b, nu):
 
 			return F_nu
 
-		case(b = 2, k = 0, beta_1 = 1/3, beta_2 = ((1-p)/2), s = (1.84-(0.40*p)), nu_b = nu_m)
 
-                        #This is for break = 2 of k = 0
-		
-                        var1 = (p - 0.67) * (10**15)
+
+
+		# Break 2 for k = 0
+		case(b = 2, k = 0, beta_1 = 1/3, beta_2 = ((1-p)/2), s = (1.84-(0.40*p)), nu_b = nu_m):
+			
+			# Dummy variables for calculating nu_2
+			var1 = (p - 0.67) * (10**15)
 			var2 = pow((1 + z), 1/2)
 			var3 = pow(E_52, 1/2)
 			var4 = pow(epsilon_e_bar, 2)
@@ -139,82 +146,34 @@ def BreakCase(b, k, beta_1, beta_2, s, nu_b, nu):
 
 			return F_tilde_2
 
-		case(b = 3, k = 0, beta_1 = ((1-p)/2), beta_2 = (-p/2), s = (1.15-(0.06*p)), nu_b = nu_c)
-			#this is for break = 3 of k = 0
-                        var1 = (p - 0.46) * (10**13)
+
+
+
+		# Break 3 for k = 0
+		case(b = 3, k = 0, beta_1 = ((1-p)/2), beta_2 = (-p/2), s = (1.15-(0.06*p)), nu_b = nu_c):
+
+			# Dummy variables for calculating nu_3
+			var1 = (p - 0.46) * (10**13)
 			var2 = math.exp(-1.16*p)
-                        var3 = pow((1 + z), -1/2)
+			var3 = pow((1 + z), -1/2)
 			var4 = pow(epsilon_B, -3/2)
 			var5 = pow(n_0, -1)
 			var6 = pow(E_52, -1/2)
 			var7 = pow(t_days, -1/2)
-		              
+					  
 			nu_3 = 6.37 * var1 * var2 * var3 * var4 * var5 * var6 * var7
 
 			F_tilde_3 = (1 + pow((nu/nu_sa), s*(beta_1 - beta_2)))**(-1/s)
 
 			return F_tilde_3
 
-		case _:
-			print("Error: Invalid option for b, k")
-			break
-			#invalid option for b, k
 
 
-#KP - Tried to implement function 5 here
-def F1(F_nu, F_tilde_2, F_tilde_3):
-	return F_nu * F_tilde_2 * F_tilde_3
-# Previous BreakCase(): without flux density equations
-'''
-def BreakCase(b,k): 
-	match(b,k):
-		case(b = 1, k = 0):
-			beta_1 = 2
-			beta_2 = 1/3
-			b = 1
-			nu_b = nu_sa
+		# DK - Still missing s, and nu_b, need to refer to Granot and Sari for these later
+		# Break 7 for k = 0
+		case(b = 7, k = 0, beta_1 = 2, beta_2 = 11/8, s = , nu_b = ):
 
-			var1 = (pow((p - 1), 3/5)) / (pow((3*p + 2), 3/5))
-			var2 = pow((1 + z), -1)
-			var3 = pow(epsilon_e_bar, -1)
-			var4 = pow(epsilon_B, 1/5)
-			var5 = pow(n_0, 3/5)
-			var6 = pow(E_52, 1/5)
-
-			nu_1 = 1.24 * var1 * (10**9) * var2 * var3 * var4 * var5 * var6
-
-		case(b = 2, k = 0):
-			beta_1 = 1/3
-			beta_2 = (1-p)/2
-			nu_b = nu_m
-
-			var1 = (p - 0.67) * (10**15)
-			var2 = pow((1 + z), 1/2)
-			var3 = pow(E_52, 1/2)
-			var4 = pow(epsilon_e_bar, 2)
-			var5 = pow(epsilon_B, 1/2)
-			var6 = pow(t_days, -3/2)
-
-			nu_2 = 3.73 * var1 * var2 * var3 * var4 * var5 * var6
-
-		case(b = 3, k = 0):
-			beta_1 = (1 - p)/2
-			beta_2 = -p/2
-			nu_b = nu_c
-
-			var1 = (p - 0.46) * (10**13)
-			var2 = pow(e, -1.16*p) # is the e here the function e or something else
-			# if it is exponential function: math.exp(-1.16*p)
-			var3 = pow((1 + z), -1/2)
-			var4 = pow(epsilon_B, -3/2)
-			var5 = pow(n_0, -1)
-			var6 = pow(E_52, -1/2)
-			var7 = pow(t_days, -1/2)
-		              
-			nu_3 = 6.37 * var1 * var2 * var3 * var4 * var5 * var6 * var7
-
-		case(b = 7, k = 0):
-			#dummy variables for writing nu_7, E.G. var_1 = var_7_1
+			#dummy variables for calculating nu_7
 			var1 = ( ((3p-1)**(8/5)) / ((3p+2)**(8/5)) )
 			var2 = (1+z)**(-13/10)
 			var3 = (epsilon_e_bar**(-8/5))
@@ -223,18 +182,18 @@ def BreakCase(b,k):
 			var6 = (E_52**(-1/10))
 			var7 = (t_day**(3/10))
 
-			#Given by table 2 for b = 7
-			beta_1 = 2
-			beta_2 = 11/8
 
-			#function given by nu_b of granot and sari
+			# Function given by nu_b of Granot and Sari
 			nu_7 = 1.12 * var1 * (10**8) * var2 * var3 * var4 * var5 * var6 * var7
 
-			#nu_b for the given case of b = 7, represents nu_ac
-			return nu_7, beta_1, beta_2, s
 
-		case(b = 9, k = 0):
-			#dummy variables to shorten nu_9
+
+
+		# DK - Still missing s, need to refer to Granot and Sari for this
+		# Break 9 for k = 0
+		case(b = 9, k = 0, beta_1 = -1/2, beta_2 = -p/2, s = , nu_b = nu_m):
+
+			#dummy variables calculating nu_9
 			var1 = (p - 0.74)
 			var2 = (1 + z)**(1/2)
 			var3 = epsilon_e_bar**2
@@ -242,28 +201,32 @@ def BreakCase(b,k):
 			var5 = E_52**(1/2)
 			var6 = t_days**(-3/2)
 
+			# Function given by nu_b of Granot and Sari
 			nu_9 = 3.94 * var1 * 10**15 * var2 * var3 * var4 * var5 * var6
-			beta_1 = -1/2
-			beta_2 = -p/2
-			nu_b = nu_m
 
-		case(b = 10, k = 0):
-			#dummy variables to shorten nu_10
+
+
+
+		# DK - Still missing s, need to refer to Granot and Sari for this
+		# Break 10 for k = 0
+		case(b = 10, k = 0, beta_1 = 11/8, beta_2 = 1/3, s = , nu_b = nu_sa):
+
+			# Dummy variables to calculate nu_10
 			var1 = (1+z)**(-1/2)
 			var2 = epsilon_B**(6/5)
 			var3 = n_0**(-1)
 			var4 = E_52**(7/10)
 			var5 = t_days**(-1/2)
 
+			# Function given by nu_b of Granot and Sari
 			nu_10 = 1.32 * 10**10 * var1 * var2 * var3 * var4 * var5 
 
-			beta_1 = 11/8
-			beta_2 = 1/3
-			nu_b = nu_sa
 
-		case(b = 11, k = 0):
-			beta_1 = 1/3
-			beta_2 = -1/2
+
+
+		# DK - Still missing s, and nu_b, need to refer to Granot and Sari for these later
+		# Break 11 for k = 0
+		case(b = 11, k = 0, beta_1 = 1/3, beta_2 = -1/2, s = , nu_b =):
 
 			#dummy variables to shorten nu_11
 			var1 = pow((1+z), -1/2)
@@ -272,13 +235,22 @@ def BreakCase(b,k):
 			var4 = pow(E_52, -1/2)
 			var5 = pow(t_days, -1/2)
 
+			# Function given by nu_b of Granot and Sari
 			nu_11 = 5.86 * 10**12 * var1 * var2 * var3 * var4 * var_11_5
 
+
+
+
+
+		# Else case, I.E. invalid option for b, k, breaks code and outputs error message
 		case _:
 			print("Error: Invalid option for b, k")
 			break
-			#invalid option for b, k
-'''
+
+
+#KP - Tried to implement equation 5 here
+def F1(F_nu, F_tilde_2, F_tilde_3):
+	return F_nu * F_tilde_2 * F_tilde_3
 
 
 
@@ -313,6 +285,3 @@ def BreakCase(b,k):
 #var_7_ext_8 = pow(d_L28, -2)
 
 #nu_7_ext = 5.27 * pow(10, -3) * var_7_ext_1 * var_7_ext_2 * var_7_ext_3 * var_7_ext_4 * var_7_ext_5 * var_7_ext_6 * var_7_ext_7 * var_7_ext_8
-
-
-
