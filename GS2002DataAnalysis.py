01a2ee4d324994b34632654891597866cc220665
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2024-11-18 20:06:42 -0500

    Add files via upload

---
+++ GS2002DataAnalysis.py
@@ -0,0 +1,68 @@
+import linecache
+import matplotlib.pyplot as plt
+
+# Random iterator numbers selected to compare previous results from GS2002_text.f90 to grba.py
+itm_numbers = [0, 27, 35, 45, 51]
+
+# Fetches the line numbers based on each itm
+def getLineNum(itm):
+
+	line_start = 1 + itm * 644
+	line_end = line_start + 641
+
+	return line_start, line_end
+
+
+# Opens fort.810 and names it HisData
+with open("./WarrenCode/fort.810", "r") as HisData:
+
+	# Seperates the data by line
+	lines=HisData.readlines() 
+
+	# Setting an empty array to define log10(Jy)
+	y = []
+
+	# Setting an empty array to define log10(Hz)
+	x = []
+
+
+	# iterates through the itm numbers
+	for i in itm_numbers:
+		# finds the start and end lines for each itm number
+		line_start, line_end = getLineNum(i)
+
+		# Creates new array in y, x for each itm_number
+		y.append([])
+		x.append([])
+
+		for j in range(line_start, line_end):
+	
+	
+			# y,x [-1] adds all of the data to the newly created arrays in the last for loop
+			# Split is going through each section of the line, with the delimiter being empty space
+	
+			y[-1].append(float(lines[j].split()[5]))
+			# Split 5 is finding log10(Jy) in fort.810
+	
+			x[-1].append(float(lines[j].split()[3]))
+			# Split 3 is finding log10(nuFnu) in fort.810
+	
+
+			# The final output of y and x is a list of lists with each being a list of values for each itm_number
+
+	HisData.close()
+
+
+# plotting stuff
+fig, ax = plt.subplots()
+ax.plot(x[0], y[0], 'k-', linewidth=2, label="iteration 0")
+ax.plot(x[1], y[1], 'r-', linewidth=2, label="iteration 27")
+ax.plot(x[2], y[2], 'b-', linewidth=2, label="iteration 35")
+ax.plot(x[3], y[3], 'g-', linewidth=2, label="iteration 45")
+ax.plot(x[4], y[4], 'm-', linewidth=2, label="iteration 51")
+plt.xscale('linear')
+ax.set_yscale('linear')
+ax.legend(loc="upper right")
+ax.set_xlabel("Log(nu) [Hz]")
+ax.set_ylabel("Log(F_nu) [Jy]")
+plt.savefig("./output/GS2002.png")
