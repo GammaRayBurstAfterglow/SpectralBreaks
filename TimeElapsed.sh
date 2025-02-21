3e8b9662b376e6d142ca13a32e87b64abda2b82b
Damien Koon <52017620+acroscopic@users.noreply.github.com>
2025-02-21 16:40:16 -0500

    Delete TimeElapsed.sh

--- TimeElapsed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Sorts through all directories recursively, which searching all of the output files for the elapsed time, then outputting into elapsed.txt
-find ~/GRBA -mindepth 2 -maxdepth 2 -type d | while read -r dir; do
-        find "$dir" -type f -name "*_out_*.dat" -exec grep "Emission calculated!" {} + | sed 's/.*time = //; s/ sec//'
-done > elapsed.txt
-
-# Sorts so head and tail show any outliars in the data
-sort elapsed.txt -o elapsed.txt
